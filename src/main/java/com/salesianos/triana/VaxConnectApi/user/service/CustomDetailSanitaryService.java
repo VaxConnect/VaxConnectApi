2f93e04bcda1e4b217763afce0e499df7cec7d05
Roberto <robertorebolledo151@gmail.com>
2023-11-27 12:36:31 +0100

    fire

--- src/main/java/com/salesianos/triana/VaxConnectApi/user/service/CustomDetailSanitaryService.java
+++ src/main/java/com/salesianos/triana/VaxConnectApi/user/service/CustomDetailSanitaryService.java
@@ -6,7 +6,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-@Primary
+
 @Service("sanitaryDetailService")
 @RequiredArgsConstructor
 public class CustomDetailSanitaryService implements UserDetailsService {
